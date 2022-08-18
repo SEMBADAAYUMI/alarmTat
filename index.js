8f7ec2d99e58dce3955dbeddad34575afe8cff2c
SEMBADAAYUMI <sembadaayumiasyrafaufa@gmail.com>
2022-08-18 17:22:04 +0700

    menambahkan suara notifikasi alarm

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const display = document.getElementById("clock");
+const audio = new Audio('./files/belKlakson.mp3');
+audio.loop = true;
+let alarmTime = null;
+let alarmTimeout = null;
+
 function updateTime() {
   const date = new Date();
   const hour = date.getHours();
@@ -15,29 +20,39 @@ function setAlarm() {
   if (alarmTime) {
     const current = new Date();
     const timeToAlarm = new Date(alarmTime);
+    
 
     if (timeToAlarm > current) {
       const timeOut = timeToAlarm.getTime() - current.getTime();
-      alarmTimeOut = setTimeout(() => audio.play(), timeOut);
+      alarmTimeout = setTimeout(() => audio.play(), timeOut);
       alert("alarm set");
     }
   }
 }
 
-const tanggalTujuan = new Date("Aug 18, 2022 14:26:00").getTime();
-
-const hitungMundur = setInterval(function () {
-  const sekarang = new Date().getTime();
-  const selisih = tanggalTujuan - sekarang;
-  const hari = ~~(selisih / (1000 * 60 * 60 * 24));
-  const jam = ~~((selisih % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  const menit = ~~((selisih % (1000 * 60 * 60)) / (1000 * 60));
-  const detik = ~~((selisih % (1000 * 60)) / 1000);
-  const tex = document.getElementById("teks");
-  tex.innerHTML = "waktu anda tinggal : " + hari + " hari " + jam + " jam " + menit + " menit " + detik + " detik lagi";
-  if (selisih < 0) {
-    clearInterval(hitungMundur);
-    alert("waktu habis");
-    tex.innerHTML = "waktu habis";
+function clearAlarm() {
+  audio.pause();
+  if(alarmTimeout) {
+    clearTimeout(alarmTimeout);
+    alert('alarm cleared');
   }
-}, 1000);
+}
+
+// let tanggalTujuan = new Date("Aug 18, 2022 14:26:00").getTime();
+
+
+// const hitungMundur = setInterval(function () {
+//   const sekarang = new Date().getTime();
+//   const selisih = tanggalTujuan - sekarang;
+//   const hari = ~~(selisih / (1000 * 60 * 60 * 24));
+//   const jam = ~~((selisih % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+//   const menit = ~~((selisih % (1000 * 60 * 60)) / (1000 * 60));
+//   const detik = ~~((selisih % (1000 * 60)) / 1000);
+//   const tex = document.getElementById("teks");
+//   tex.innerHTML = "waktu anda tinggal : " + hari + " hari " + jam + " jam " + menit + " menit " + detik + " detik lagi";
+//   if (selisih < 0) {
+//     clearInterval(hitungMundur);
+//     alert("waktu habis");
+//     tex.innerHTML = "waktu habis";
+//   }
+// }, 1000);
