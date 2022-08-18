b45cdadada6fd2039bffb446b88736160e8c529b
SEMBADAAYUMI <sembadaayumiasyrafaufa@gmail.com>
2022-08-19 01:56:56 +0700

    menambahkan nav-bar

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const display = document.getElementById("clock");
-const audio = new Audio('./files/belKlakson.mp3');
+const audio = new Audio("./files/belKlakson.mp3");
 audio.loop = true;
 let alarmTime = null;
 let alarmTimeout = null;
 
 function updateTime() {
   const date = new Date();
-  const hour = date.getHours();
-  const minutes = date.getMinutes();
-  const seconds = date.getSeconds();
+  const hour = formatTime(date.getHours());
+  const minutes = formatTime(date.getMinutes());
+  const seconds = formatTime(date.getSeconds());
   display.innerText = `${hour} : ${minutes} : ${seconds}`;
+}
+function formatTime(time) {
+  if (time < 10) {
+    return "0" + time;
+  }
+  return time;
 }
 setInterval(updateTime, 1000);
 
@@ -20,7 +26,6 @@ function setAlarm() {
   if (alarmTime) {
     const current = new Date();
     const timeToAlarm = new Date(alarmTime);
-    
 
     if (timeToAlarm > current) {
       const timeOut = timeToAlarm.getTime() - current.getTime();
@@ -32,27 +37,25 @@ function setAlarm() {
 
 function clearAlarm() {
   audio.pause();
-  if(alarmTimeout) {
+  if (alarmTimeout) {
     clearTimeout(alarmTimeout);
-    alert('alarm cleared');
+    alert("alarm cleared");
   }
 }
 
-// let tanggalTujuan = new Date("Aug 18, 2022 14:26:00").getTime();
-
-
-// const hitungMundur = setInterval(function () {
-//   const sekarang = new Date().getTime();
-//   const selisih = tanggalTujuan - sekarang;
-//   const hari = ~~(selisih / (1000 * 60 * 60 * 24));
-//   const jam = ~~((selisih % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-//   const menit = ~~((selisih % (1000 * 60 * 60)) / (1000 * 60));
-//   const detik = ~~((selisih % (1000 * 60)) / 1000);
-//   const tex = document.getElementById("teks");
-//   tex.innerHTML = "waktu anda tinggal : " + hari + " hari " + jam + " jam " + menit + " menit " + detik + " detik lagi";
-//   if (selisih < 0) {
-//     clearInterval(hitungMundur);
-//     alert("waktu habis");
-//     tex.innerHTML = "waktu habis";
-//   }
-// }, 1000);
+const tanggalTujuan = new Date("Aug 19, 2022 23:26:00").getTime();
+const hitungMundur = setInterval(function () {
+  const sekarang = new Date().getTime();
+  const selisih = tanggalTujuan - sekarang;
+  const hari = ~~(selisih / (1000 * 60 * 60 * 24));
+  const jam = ~~((selisih % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const menit = ~~((selisih % (1000 * 60 * 60)) / (1000 * 60));
+  const detik = ~~((selisih % (1000 * 60)) / 1000);
+  const tex = document.getElementById("teks");
+  tex.innerHTML = "waktu anda tinggal : " + hari + " hari " + jam + " jam " + menit + " menit " + detik + " detik lagi";
+  if (selisih < 0) {
+    clearInterval(hitungMundur);
+    alert("waktu habis");
+    tex.innerHTML = "waktu habis";
+  }
+}, 1000);
